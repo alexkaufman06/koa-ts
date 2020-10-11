71f780f2e4b23909271b4b30e06aec102502e3b9
alexkaufman06 <alexkaufman06@gmail.com>
2020-10-10 20:21:29 -0700

    Add healthcheck test

---
+++ test/routes/healthcheck.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import server from "../../src/server";
+
+// close the server after each test
+afterEach((done) => {
+  server.close();
+  done();
+});
+
+describe("routes/healthcheck", () => {
+  it("return data", async () => {
+    const response = await request(server).get("/healthcheck");
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body.data).toEqual("It works!");
+  });
+});
